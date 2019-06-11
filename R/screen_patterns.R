c10935a202f0b2fa4ad4a63aa4ad13839080c9a3
Elena Khusainova <erhusainova@gmail.com>
2019-06-11 15:02:14 -0400

    Add files via upload
    added metrics

--- R/screen_patterns.R
+++ R/screen_patterns.R
@@ -3,24 +3,26 @@
 #' The function screens for the available patterns to pick best ones based
 #' on the information gain
 #'
-#' @usage .screen_patterns(data, classes, pattern_pool, num_pat_max = 2000)
+#' @usage .screen_patterns(data, classes, pattern_pool, num_pat_max = 2000, metric = "cond.entropy")
 #'
 #' @param data a data frame containing the variables in the model
 #' @param classes a vector of classes (0 or 1) for the data.
 #' @param pattern_pool the set of all possible patterns for \code{pattern_set} mined from the data
 #' @param num_pat_max how many patterns to keep
+#' @param metric metric to use for impurity measure of a split. Possible values: "cond.entropy", "Gini"
 #' @return a list of all patterns in the given data, indexed by lengths
 #' @keywords internal
 #' 
 
-.screen_patterns <- function(data, classes, pattern_pool, num_pat_max = 2000) {
+.screen_patterns <- function(data, classes, pattern_pool, num_pat_max = 2000,
+                             metric = "cond.entropy") {
 
-  entropies <- entropies(data, classes, pattern_pool)
+  impScrs <- impurityScores(data, classes, pattern_pool, metric)
 
-  threshold <- ifelse(num_pat_max < length(entropies),
-                      sort(entropies)[num_pat_max],
-                      max(entropies))
-  patterns <- pattern_pool[entropies <= threshold]
+  threshold <- ifelse(num_pat_max < length(impScrs),
+                      sort(impScrs)[num_pat_max],
+                      max(impScrs))
+  patterns <- pattern_pool[impScrs <= threshold]
   pattern_pool <- lapply(1:max(lengths(patterns)),
                          FUN = function(l) {
                            unname(patterns[lengths(patterns) == l])
